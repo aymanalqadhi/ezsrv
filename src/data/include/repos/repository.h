b643032f6f94dbf57987d193e779e0241e49a374
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-12 16:03:20 +0300

    Make all base repository class methods pure virtual methods

--- src/data/include/repos/repository.h
+++ src/data/include/repos/repository.h
@@ -8,20 +8,20 @@ namespace ezsrv::data::repos {
     template <typename TModel>
     class repository_base {
       public:
-        virtual bool add(const TModel &item);
-        virtual bool add(const std::vector<TModel> &items);
+        virtual bool add(const TModel &item) = 0;
+        virtual bool add(const std::vector<TModel> &items) = 0;
 
-        virtual bool get(std::uint32_t id, TModel& out);
+        virtual bool get(std::uint32_t id, TModel& out) = 0;
         virtual std::vector<TModel> get(
-            std::function<bool(const TModel &)> pred);
-        virtual std::vector<TModel> get_all();
+            std::function<bool(const TModel &)> pred) = 0;
+        virtual std::vector<TModel> get_all() = 0;
 
-        virtual bool update(const TModel &item);
+        virtual bool update(const TModel &item) = 0;
 
-        virtual bool remove(std::uint32_t id);
-        virtual bool remove(std::function<bool(const TModel &)> pred);
+        virtual bool remove(std::uint32_t id) = 0;
+        virtual bool remove(std::function<bool(const TModel &)> pred) = 0;
 
-        virtual std::uint64_t count();
+        virtual std::uint64_t count() = 0;
     };
 } // namespace ezsrv::data
 
