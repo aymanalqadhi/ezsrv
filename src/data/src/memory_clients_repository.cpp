07916c66323b5cdacbe5ab1cf0dec29ff066c55c
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-12 14:34:57 +0300

    Implement the in-memory clients repository

---
+++ src/data/src/memory_clients_repository.cpp
@@ -0,0 +1,85 @@
+#include "repos/memory_clients_repository.h"
+
+#include "models/client.h"
+
+#include <vector>
+
+using ezsrv::data::models::client;
+using ezsrv::data::repos::memory_clients_repository;
+
+bool memory_clients_repository::add(const client &item) {
+    clients_.push_back(item);
+    return true;
+}
+
+bool memory_clients_repository::add(const std::vector<client> &items) {
+    clients_.insert(clients_.end(), items.begin(), items.end());
+    return true;
+}
+
+bool memory_clients_repository::get(std::uint32_t id, client& out) {
+    auto client = std::find(clients_.begin(), clients_.end(),
+        [id](const auto &c) { return c.id() == id; });
+
+    if (client == clients_.end()) {
+        return true;
+    }
+
+    out = *client;
+    return true;
+}
+
+std::vector<client> memory_clients_repository::get(
+    std::function<bool(const client &)> pred) {
+    std::vector<client> ret;
+    for (const auto &client : clients_) {
+        if (pred(client)) {
+            ret.push_back(client);
+        }
+    }
+    return ret;
+}
+
+std::vector<client> memory_clients_repository::get_all() {
+    return clients_;
+}
+
+bool memory_clients_repository::update(const client &item) {
+    auto client = std::find(clients_.begin(), clients_.end(),
+        [id = item.id()](const auto &c) { return c.id() == id; });
+
+    if (client == clients_.end()) {
+        return false;
+    }
+
+    client->type(item.type());
+    client->name(item.name());
+    client->permissions(item.permissions());
+
+    return true;
+}
+
+bool memory_clients_repository::remove(std::uint32_t id) {
+    auto client = std::find(clients_.begin(), clients_.end(),
+        [id](const auto &c) { return c.id() == id; });
+
+    if (client == clients_.end()) {
+        return false;
+    }
+
+    clients_.erase(client);
+    return true;
+}
+
+bool memory_clients_repository::remove(std::function<bool (const client &)> pred) {
+    for (auto itr = clients_.begin(); itr != clients_.end(); ++itr) {
+        if (pred(*itr)) {
+            clients_.erase(itr);
+        }
+    }
+    return true;
+}
+
+std::uint64_t memory_clients_repository::count() {
+    return clients_.size();
+}
