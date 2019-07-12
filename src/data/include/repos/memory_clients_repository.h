045365cb21994207f56499933c36dee9944d27c3
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-12 14:33:52 +0300

    Add an in-memory clients repository

---
+++ src/data/include/repos/memory_clients_repository.h
@@ -0,0 +1,35 @@
+#ifndef EZSRV_DATA_REPOS_MEMORY_CLIENTS_REPOSITORY_H
+#define EZSRV_DATA_REPOS_MEMORY_CLIENTS_REPOSITORY_H
+
+#include "models/client.h"
+#include "repos/repository.h"
+
+#include <vector>
+
+namespace ezsrv::data::repos {
+    class memory_clients_repository
+        : public repository_base<ezsrv::data::models::client> {
+        using client = ezsrv::data::models::client;
+
+      public:
+        bool add(const client &item) override;
+        bool add(const std::vector<client> &items) override;
+
+        bool get(std::uint32_t id, client& out) override;
+        std::vector<client> get_all() override;
+        std::vector<client> get(
+            std::function<bool(const client &)> pred) override;
+
+        bool update(const client &item) override;
+
+        bool remove(std::uint32_t id) override;
+        bool remove(std::function<bool(const client &)> pred) override;
+
+        std::uint64_t count() override;
+
+      private:
+        std::vector<client> clients_;
+    };
+} // namespace ezsrv::data::repos
+
+#endif /* EZSRV_DATA_REPOS_MEMORY_CLIENTS_REPOSITORY_H */
