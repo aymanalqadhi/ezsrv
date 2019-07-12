7252f9bff1c3d1b1f194ef0ef8c009f65c72ab3c
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-12 17:54:44 +0300

    Add a structure to hold the configuration values
    * Add constants for application global information
    * Add constants for configuration defaults
    * Add a static function to parse configuration from command line
    arguments
    * Add a static function to parse configuration from configuration file

---
+++ src/config/include/config/app_config.h
@@ -0,0 +1,29 @@
+#ifndef EZSRV_CONFIG_APP_CONFIG_H
+#define EZSRV_CONFIG_APP_CONFIG_H
+
+#include <string>
+
+namespace ezsrv::config {
+    namespace info {
+        constexpr char app_name[] {"ezsrv"};
+        constexpr char app_description[] {
+            "A high performance realtime communication server"};
+        constexpr char app_version[] {"v0.1 ALPHA"};
+    } // namespace info
+
+    namespace defaults {
+        constexpr std::uint16_t listen_port {3434};
+        constexpr std::uint8_t  verbosity {0};
+    } // namespace defaults
+
+    struct app_config {
+        const std::string   executable;
+        const std::uint16_t listen_port;
+        const std::uint8_t  verbosity;
+
+        static app_config from_command_line(int argc, const char *argv[]);
+        static app_config from_config_file(const std::string &path);
+    };
+} // namespace ezsrv::config
+
+#endif /* EZSRV_CONFIG_APP_CONFIG_H */
