854ff886eea6529c93a7bc8177dfb14775822e68
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-15 20:07:32 +0300

    Parse command line arguments, and create and run an ezsrv_app instance

--- src/main/src/main.cpp
+++ src/main/src/main.cpp
@@ -1,7 +1,33 @@
+#include "config/app_config.h"
+
+#include "ezsrv_app.h"
+#include "iapplication.h"
+
+#include "boost/di.hpp"
+
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+using ezsrv::app::ezsrv_app;
+using ezsrv::app::iapplication;
+
+using ezsrv::config::app_config;
+using ezsrv::log::logger;
+
+int main(int argc, const char *argv[]) {
+    try {
+        auto opts {app_config::from_command_line(argc, argv)};
+
+        const auto injector = boost::di::make_injector(
+            boost::di::bind<app_config>().to(opts),
+            boost::di::bind<iapplication>().to<ezsrv_app>());
+        auto app = injector.create<ezsrv_app>();
+
+        app.configure();
+        app.run();
+    } catch (const std::exception &ex) {
+        std::cerr << ex.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
