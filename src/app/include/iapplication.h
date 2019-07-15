9eaf6e6fa8cb67ecc7223bcc2ccaf7bb3c7190ed
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-15 20:01:50 +0300

    Change configure method signature and add a virtual destructor

--- src/app/include/iapplication.h
+++ src/app/include/iapplication.h
@@ -1,13 +1,13 @@
 #ifndef EZSRV_APP_IAPPLICATION_H
 #define EZSRV_APP_IAPPLICATION_H
 
-#include "config/app_config.h"
-
 namespace ezsrv::app {
     class iapplication {
       public:
-        virtual void configure(const ezsrv::config::app_config& config) = 0;
+        virtual void configure() = 0;
         virtual void run() = 0;
+
+        virtual ~iapplication() noexcept = default;
     };
 } // namespace ezsrv::app
 
