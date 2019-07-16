b0093465441d871101ebffc86fae93babdeccb68
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-16 20:18:20 +0300

    Add the data source to the dependency injection container

--- src/main/src/main.cpp
+++ src/main/src/main.cpp
@@ -1,4 +1,6 @@
 #include "config/app_config.h"
+#include "data/sources/ezsrv_data_source.h"
+#include "data/sources/memory_ezsrv_data_source.h"
 
 #include "ezsrv_app.h"
 #include "iapplication.h"
@@ -7,19 +9,23 @@
 
 #include <iostream>
 
+namespace di = boost::di;
+
 using ezsrv::app::ezsrv_app;
 using ezsrv::app::iapplication;
-
 using ezsrv::config::app_config;
+using ezsrv::data::sources::ezsrv_data_source_base;
+using ezsrv::data::sources::memory_ezsrv_data_source;
 using ezsrv::log::logger;
 
 int main(int argc, const char *argv[]) {
     try {
         auto opts {app_config::from_command_line(argc, argv)};
 
-        const auto injector = boost::di::make_injector(
-            boost::di::bind<app_config>().to(opts),
-            boost::di::bind<iapplication>().to<ezsrv_app>());
+        const auto injector = di::make_injector(
+            di::bind<app_config>().to(opts),
+            di::bind<ezsrv_data_source_base>().to<memory_ezsrv_data_source>(),
+            di::bind<iapplication>().to<ezsrv_app>());
         auto app = injector.create<iapplication *>();
 
         app->configure();
