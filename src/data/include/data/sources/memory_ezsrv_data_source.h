c0488643bdc59a450a63ff34956aedf1edb475b2
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-16 20:32:19 +0300

    Add a root account at the source construction

--- src/data/include/data/sources/memory_ezsrv_data_source.h
+++ src/data/include/data/sources/memory_ezsrv_data_source.h
@@ -6,15 +6,23 @@
 
 namespace ezsrv::data::sources {
     class memory_ezsrv_data_source : public ezsrv_data_source_base {
-        using client       = ezsrv::data::models::client;
-        using clients_repo = ezsrv::data::repos::memory_clients_repository;
+        using client           = ezsrv::data::models::client;
+        using clients_repo     = ezsrv::data::repos::memory_clients_repository;
+        using permission_group = ezsrv::data::models::client_permission_group;
+        using client_type      = ezsrv::data::models::client_type;
 
         template <typename TModel>
         using repository = ezsrv::data::repos::repository_base<TModel>;
 
       public:
         memory_ezsrv_data_source()
-            : clients_repo_ {std::make_shared<clients_repo>()} {}
+            : clients_repo_ {std::make_shared<clients_repo>()} {
+            client root {0};
+            root.set_permission_group(permission_group::root);
+            root.name("root");
+            root.type(client_type::root);
+            clients_repo_->add(std::move(root));
+        }
 
         std::shared_ptr<repository<client>> clients_repository() override {
             return clients_repo_;
