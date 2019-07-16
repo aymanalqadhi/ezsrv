8309fb0e1eadc966904d00e155b39ab09358ce95
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-16 20:33:45 +0300

    Add a placeholder implementation for authenticate method

--- src/data/src/memory_clients_repository.cpp
+++ src/data/src/memory_clients_repository.cpp
@@ -2,6 +2,8 @@
 
 #include "data/models/client.h"
 
+#include <memory>
+#include <string_view>
 #include <vector>
 
 using ezsrv::data::models::client;
@@ -18,8 +20,9 @@ bool memory_clients_repository::add(const std::vector<client> &items) {
 }
 
 bool memory_clients_repository::get(std::uint32_t id, client &out) {
-    auto client = std::find_if(clients_.begin(), clients_.end(),
-        [id](const auto &c) -> bool { return c.id() == id; });
+    auto client =
+        std::find_if(clients_.begin(), clients_.end(),
+                     [id](const auto &c) -> bool { return c.id() == id; });
 
     if (client == clients_.end()) {
         return false;
@@ -29,8 +32,8 @@ bool memory_clients_repository::get(std::uint32_t id, client &out) {
     return true;
 }
 
-std::vector<client> memory_clients_repository::get(
-    std::function<bool(const client &)> pred) {
+std::vector<client>
+memory_clients_repository::get(std::function<bool(const client &)> pred) {
     std::vector<client> ret;
     for (const auto &client : clients_) {
         if (pred(client)) {
@@ -45,8 +48,9 @@ std::vector<client> memory_clients_repository::get_all() {
 }
 
 bool memory_clients_repository::update(const client &item) {
-    auto client = std::find_if(clients_.begin(), clients_.end(),
-        [id = item.id()](const auto &c) { return c.id() == id; });
+    auto client =
+        std::find_if(clients_.begin(), clients_.end(),
+                     [id = item.id()](const auto &c) { return c.id() == id; });
 
     if (client == clients_.end()) {
         return false;
@@ -61,7 +65,7 @@ bool memory_clients_repository::update(const client &item) {
 
 bool memory_clients_repository::remove(std::uint32_t id) {
     auto client = std::find_if(clients_.begin(), clients_.end(),
-        [id](const auto &c) { return c.id() == id; });
+                               [id](const auto &c) { return c.id() == id; });
 
     if (client == clients_.end()) {
         return false;
@@ -83,4 +87,22 @@ bool memory_clients_repository::remove(
 
 std::uint64_t memory_clients_repository::count() {
     return clients_.size();
+}
+
+bool memory_clients_repository::authenticate(std::string_view        username,
+                                             std::string_view        password,
+                                             std::shared_ptr<client> out) {
+    if (username == "root" && password == "123") {
+        client root_client;
+        get(0, root_client);
+
+        out->id(root_client.id());
+        out->name(root_client.name());
+        out->permissions(root_client.permissions());
+        out->type(root_client.type());
+
+        return true;
+    }
+
+    return false;
 }
