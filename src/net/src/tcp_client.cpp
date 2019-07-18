d1a1cc4b0dfaf763e244bbd663808ee910e772f5
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-18 21:30:09 +0300

    Implement client start method

--- src/net/src/tcp_client.cpp
+++ src/net/src/tcp_client.cpp
@@ -5,16 +5,27 @@
 #include "boost/asio/placeholders.hpp"
 #include "boost/asio/read.hpp"
 
-using ezsrv::net::auth_message_length;
 using ezsrv::net::tcp_client;
+using ezsrv::net::header_size;
 
 using boost::asio::placeholders::bytes_transferred;
 using boost::asio::placeholders::error;
 
 void tcp_client::start() {
-}
+    using namespace std::placeholders;
 
-void tcp_client::on_authing(std::string_view msg) {
+    if (state() == client_state::idle) {
+        state(client_state::reading_header);
+        tmp_buffer_.reserve(header_size);
+
+        boost::asio::async_read(
+            sock_, boost::asio::buffer(tmp_buffer_),
+            boost::asio::transfer_exactly(header_size),
+            std::bind(&tcp_client::handle_read, shared_from_this(), _1,
+                      tmp_buffer_, _2));
+    } else {
+        throw std::runtime_error {"Client was already started!"};
+    }
 }
 
 void tcp_client::on_reading_header(std::string_view msg) {
