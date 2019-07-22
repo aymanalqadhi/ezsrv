4f51a3acf2de2e8aa6a792b45fc0bf27fecb3f11
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-22 23:55:10 +0300

    Rewritten the request message header
    The header now consists of:
    1. 2 bytes for the request type
    2. 2 bytes for the extra info that might be embbeded with the request
    for a specific reuqest type
    3. 4 bytes for the request sequence number (used to keep track of
    conversation)
    4. 4 bytes for the request flags and options
    5. 4 bytes for the length of the following body data

--- src/net/include/net/message_header.h
+++ src/net/include/net/message_header.h
@@ -4,12 +4,27 @@
 #include <cstddef>
 #include <cstdint>
 
+#include <array>
+
 namespace ezsrv::net {
-    constexpr auto header_size {12};
+    constexpr auto header_size {16};
+
     struct request_message_header {
+        using header_buffer = std::array<std::uint8_t, header_size>;
+
+        std::uint16_t type;
+        std::uint16_t extra;
+        std::uint32_t seq_no;
         std::uint32_t flags;
-        std::uint64_t body_size;
+        std::uint32_t body_size;
+
+        static void encode(const request_message_header &header,
+                           header_buffer &               out);
+        static void decode(const header_buffer &   arr,
+                           request_message_header &out);
     };
+
+    static_assert(sizeof(request_message_header) == header_size);
 } // namespace ezsrv::net
 
 #endif /* EZSRV_NET_MESSAGE_HEADER */
