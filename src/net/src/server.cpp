e647f99d97aacc481d8f6f7dbfb90a2c0a39edd5
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-23 00:34:14 +0300

    Add basic implementation for the callbacks methods

--- src/net/src/server.cpp
+++ src/net/src/server.cpp
@@ -1,6 +1,13 @@
 #include "net/server.h"
+#include "net/tcp_client.h"
 
+#include "boost/system/error_code.hpp"
+
+using tcp_client_ptr = std::shared_ptr<ezsrv::net::tcp_client>;
 using ezsrv::net::server;
+
+using boost::asio::io_context;
+using boost::system::error_code;
 
 void server::run() {
     logger_.info("Starting Listener");
@@ -11,4 +18,17 @@ void server::run() {
 
     logger_.debug("Starting event loop");
     io_ctx_.run();
+}
+
+void server::on_message_read(const tcp_client_ptr &client,
+                             std::string_view      msg) {
+    logger_.info("Got message from {}: {}", client->address(), msg);
+}
+
+void server::on_error(const tcp_client_ptr &client, const error_code &err) {
+    logger_.info("Got error from {}: {}", client->address(), err.message());
+}
+
+void server::on_close(const tcp_client_ptr &client) {
+    logger_.info("Connection to {}:{} was closed", client->endpoint_string());
 }
