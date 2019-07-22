084faa86d4b7feb3bbbebca18c38e664883277b4
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-22 23:48:24 +0300

    Make virtual methods protected, and handle_read method private
    Other Changes:
    * Execure on_error method with an end of file error if the recieved data
    length is equal to 0

--- src/net/include/net/client_state_machine.h
+++ src/net/include/net/client_state_machine.h
@@ -1,33 +1,40 @@
 #ifndef EZSRV_NET_CLIENT_STATE_MACHINE_H
 #define EZSRV_NET_CLIENT_STATE_MACHINE_H
 
+#include "boost/asio/error.hpp"
 #include "boost/system/error_code.hpp"
 
+#include <iostream>
+
 namespace ezsrv::net {
-    enum class client_state {
-        idle,
-        reading_header,
-        reading_body,
-        closing
-    };
+    enum class client_state { idle, reading_header, reading_body, closing };
 
     namespace details {
         using boost::system::error_code;
 
         class client_state_machine {
-          public:
+          protected:
             virtual void on_reading_header(std::string_view msg) = 0;
             virtual void on_reading_body(std::string_view msg)   = 0;
             virtual void on_error(const error_code &err)         = 0;
 
+            inline client_state state() const noexcept {
+                return current_state_;
+            }
+
+            inline void state(client_state state) noexcept {
+                current_state_ = state;
+            }
+
+          private:
             void handle_read(std::size_t       nread,
                              std::string_view  msg,
                              const error_code &err) {
-                if (nread == 0) {
-                    return;
-                } else if (err) {
+                if (err) {
                     on_error(err);
                     return;
+                } else if (nread == 0) {
+                    on_error(boost::asio::error::eof);
                 }
 
                 switch (current_state_) {
@@ -42,14 +49,6 @@ namespace ezsrv::net {
                 default:
                     break;
                 }
-            }
-
-            inline client_state state() const noexcept {
-                return current_state_;
-            }
-
-            inline void state(client_state state) noexcept {
-                current_state_ = state;
             }
 
           private:
