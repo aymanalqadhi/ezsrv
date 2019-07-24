4b81784b50c198d55d40b7bf1d441586656e7dad
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-24 22:07:36 +0300

    Add utility inline functions

--- src/net/include/net/reading_context.h
+++ src/net/include/net/reading_context.h
@@ -10,19 +10,37 @@
 namespace ezsrv::net {
     class reading_context {
       public:
-        const request_message_header &header() const noexcept {
+        static constexpr std::size_t recommended_chunk_size = 1;
+
+        inline const request_message_header &header() const noexcept {
             return header_;
         }
 
-        void reset() { body_buffer_.reset(0); }
-
-        void set_header(request_message_header header) {
+        inline void set_header(request_message_header header) {
             body_buffer_.reset(header.body_size);
             header_ = std::move(header);
         }
 
-        bool add_body_data(std::string_view str) {
-            return body_buffer_.add(str);
+        inline bool add_body_data(std::string_view str) {
+            return body_buffer_.add(std::move(str));
+        }
+
+        inline bool available() const noexcept {
+            return body_buffer_.available();
+        }
+
+        inline bool is_full() const noexcept {
+            return body_buffer_.current() == body_buffer_.total();
+        }
+
+        inline std::string_view body() const noexcept {
+            return body_buffer_.data();
+        }
+
+        inline std::size_t next_chunk_size() const noexcept {
+            return body_buffer_.available() >= recommended_chunk_size
+                       ? recommended_chunk_size
+                       : body_buffer_.available();
         }
 
       private:
