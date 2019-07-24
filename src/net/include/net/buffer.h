50fe484eae26e53d0fa1c722772f299583053ba7
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-24 22:05:36 +0300

    Use a dynamically allocated buffer instead of std::string in the buffer class

--- src/net/include/net/buffer.h
+++ src/net/include/net/buffer.h
@@ -4,25 +4,23 @@
 #include <cstddef>
 #include <cstring>
 
-#include <string>
+#include <cstring>
 #include <string_view>
 
 namespace ezsrv::net {
     class basic_buffer {
       public:
+        ~basic_buffer() {
+            if (data_ != nullptr) {
+                delete[] data_;
+            }
+        }
+
         inline void allocate(std::size_t len) {
             if (data_ != nullptr) {
                 delete[] data_;
             }
             data_ = new char[len_ = len];
-        }
-
-        inline void clean() {
-            if (data_ != nullptr) {
-                delete[] data_;
-                data_ = nullptr;
-            }
-            len_ = 0;
         }
 
         inline char *      data() const noexcept { return data_; }
@@ -36,42 +34,58 @@ namespace ezsrv::net {
     class buffer {
       public:
         buffer(std::size_t length) : total_ {length}, current_ {0} {
-            buffer_.reserve(length);
+            if (length > 0) {
+                buffer_ = new std::uint8_t[length];
+            } else {
+                buffer_ = nullptr;
+            }
         }
-        buffer() : buffer(0) {};
+
+        buffer() : buffer {0} {};
+        ~buffer() {
+            if (buffer_ != nullptr) {
+                delete[] buffer_;
+            }
+        }
 
         inline std::size_t current() const noexcept { return current_; }
         inline std::size_t total() const noexcept { return total_; }
         inline std::size_t available() const noexcept {
             return total_ - current_;
         }
-        inline std::string_view data() { return {buffer_.c_str(), current_}; }
+        inline std::string_view data() const {
+            return {reinterpret_cast<char *>(buffer_), total_};
+        }
 
         inline bool add(std::string_view str) {
-            if (available() < str.length()) {
+            if (buffer_ == nullptr || available() < str.length()) {
                 return false;
             }
 
-            buffer_.append(str);
+            std::memcpy(buffer_ + current_, str.data(), str.length());
             current_ += str.length();
 
             return true;
         }
 
-        inline void reset(std::size_t len = 0) noexcept {
-            if (len > 0) {
-                buffer_.resize(len);
-                total_   = len;
+        inline void reset(std::size_t len = 0) {
+            if (len == total_) {
                 current_ = 0;
-            } else {
-                buffer_.resize(0);
-                total_ = current_ = 0;
+                return;
             }
+
+            if (buffer_ != nullptr) {
+                delete[] buffer_;
+            }
+
+            current_ = 0;
+            total_   = len;
+            buffer_  = len != 0 ? new std::uint8_t[len] : nullptr;
         }
 
       private:
-        std::size_t total_, current_;
-        std::string buffer_;
+        std::size_t   total_, current_;
+        std::uint8_t *buffer_;
     };
 } // namespace ezsrv::net
 
