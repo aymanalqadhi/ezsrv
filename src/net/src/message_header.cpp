20c4c32e65787d250ca9d977b706299679a4548a
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-26 02:15:21 +0300

    Add support for enum types in the header

--- src/net/src/message_header.cpp
+++ src/net/src/message_header.cpp
@@ -6,11 +6,12 @@
 #include <cstring>
 
 #include <array>
+#include <type_traits>
 
 namespace nd = boost::endian;
 
 using ezsrv::net::request_message_header;
-using header_buffer = std::array<std::uint8_t, ezsrv::net::header_size>;
+using header_buffer = std::array<std::uint8_t, request_message_header::size>;
 
 #include <iostream>
 namespace {
@@ -22,15 +23,27 @@ namespace {
 
     template <typename T>
     inline void extract_number(const char **buffer, T &out) {
-        out = nd::big_to_native(*reinterpret_cast<const T *>(*buffer));
+        if constexpr (std::is_enum<T>()) {
+            out = static_cast<T>(nd::big_to_native(
+                *reinterpret_cast<const std::underlying_type_t<T> *>(*buffer)));
+        } else {
+            out = nd::big_to_native(*reinterpret_cast<const T *>(*buffer));
+        }
+
         *buffer += sizeof(T);
+    }
+
+    template <typename T>
+    constexpr auto to_underlying(T e) noexcept {
+        return static_cast<std::underlying_type_t<T>>(e);
     }
 } // namespace
 
 void request_message_header::encode(const request_message_header &header,
                                     header_buffer &               out) {
     auto ptr = reinterpret_cast<char *>(out.data());
-    append_number(&ptr, nd::native_to_big(header.type));
+
+    append_number(&ptr, nd::native_to_big(to_underlying(header.type)));
     append_number(&ptr, nd::native_to_big(header.extra));
     append_number(&ptr, nd::native_to_big(header.seq_no));
     append_number(&ptr, nd::native_to_big(header.flags));
@@ -40,6 +53,7 @@ void request_message_header::encode(const request_message_header &header,
 void request_message_header::decode(const header_buffer &   arr,
                                     request_message_header &out) {
     auto ptr = reinterpret_cast<const char *>(arr.data());
+
     extract_number(&ptr, out.type);
     extract_number(&ptr, out.extra);
     extract_number(&ptr, out.seq_no);
