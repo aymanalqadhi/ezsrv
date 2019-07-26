1e9d265704e329a554f7191e7b26336b3fcd5024
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-26 22:48:13 +0300

    Add structure response_message_header represent a response message header
    The response header will be 16-byte structure, containing the following
    fields:
    1. 2 bytes: The type of the request which this response is for
    2. 2 bytes: The response code
    3. 4 bytes: The conversation sequence number (set in the request by client)
    4. 4 bytes: The response flags
    5. 4 bytes: The response body size (0 for empty)

--- src/net/include/net/message_header.h
+++ src/net/include/net/message_header.h
@@ -30,8 +30,28 @@ namespace ezsrv::net {
                            request_message_header &out);
     };
 
+    struct response_message_header {
+        static constexpr auto size {16};
+
+        using header_buffer = std::array<std::uint8_t, size>;
+
+        message_type  type;
+        std::uint16_t code;
+        std::uint32_t seq_no;
+        std::uint32_t flags;
+        std::uint32_t body_size;
+
+        static void encode(const response_message_header &header,
+                           header_buffer &                out);
+        static void decode(const header_buffer &    arr,
+                           response_message_header &out);
+    };
+
     static_assert(sizeof(request_message_header) ==
                   request_message_header::size);
+
+    static_assert(sizeof(response_message_header) ==
+                  response_message_header::size);
 } // namespace ezsrv::net
 
 #endif /* EZSRV_NET_MESSAGE_HEADER */
