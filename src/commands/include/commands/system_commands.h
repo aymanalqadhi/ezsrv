d574d2c85a72d5d182b11ab25ba901e2b3896745
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-26 21:26:40 +0300

    Add a base system command interface, and system commands container

---
+++ src/commands/include/commands/system_commands.h
@@ -0,0 +1,28 @@
+#ifndef EZSRV_COMMANDS_SYSTEM_COMMANDS_H
+#define EZSRV_COMMANDS_SYSTEM_COMMANDS_H
+
+#include "commands/command_result.h"
+
+#include <cstring>
+
+#include <map>
+#include <string_view>
+
+namespace ezsrv::commands {
+    enum class system_commands : std::uint16_t { };
+
+    class system_command {
+      public:
+        virtual command_result operator()(std::string_view msg) = 0;
+    };
+
+    class system_commands_container {
+      public:
+        system_command &get_command(system_commands command);
+
+      private:
+        std::map<system_commands, system_command> commands_;
+    };
+} // namespace ezsrv::commands
+
+#endif /* EZSRV_COMMANDS_SYSTEM_COMMANDS_H */
