0bb214f5567ebd1c5a09506c1c5e975eb9db7fa9
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-26 22:54:38 +0300

    Implement response_message_header::encode and response_message_header::decode static functions

--- src/net/src/message_header.cpp
+++ src/net/src/message_header.cpp
@@ -11,6 +11,8 @@
 namespace nd = boost::endian;
 
 using ezsrv::net::request_message_header;
+using ezsrv::net::response_message_header;
+
 using header_buffer = std::array<std::uint8_t, request_message_header::size>;
 
 #include <iostream>
@@ -56,6 +58,28 @@ void request_message_header::decode(const header_buffer &   arr,
 
     extract_number(&ptr, out.type);
     extract_number(&ptr, out.extra);
+    extract_number(&ptr, out.seq_no);
+    extract_number(&ptr, out.flags);
+    extract_number(&ptr, out.body_size);
+}
+
+void response_message_header::encode(const response_message_header &header,
+                                    header_buffer &               out) {
+    auto ptr = reinterpret_cast<char *>(out.data());
+
+    append_number(&ptr, nd::native_to_big(to_underlying(header.type)));
+    append_number(&ptr, nd::native_to_big(header.code));
+    append_number(&ptr, nd::native_to_big(header.seq_no));
+    append_number(&ptr, nd::native_to_big(header.flags));
+    append_number(&ptr, nd::native_to_big(header.body_size));
+}
+
+void response_message_header::decode(const header_buffer &   arr,
+                                    response_message_header &out) {
+    auto ptr = reinterpret_cast<const char *>(arr.data());
+
+    extract_number(&ptr, out.type);
+    extract_number(&ptr, out.code);
     extract_number(&ptr, out.seq_no);
     extract_number(&ptr, out.flags);
     extract_number(&ptr, out.body_size);
