bd151444a2d080be7e90cc157bd00364e46defb7
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-26 22:56:58 +0300

    Add response_message structure

--- src/net/include/net/message.h
+++ src/net/include/net/message.h
@@ -4,11 +4,17 @@
 #include "net/message_header.h"
 
 #include <string_view>
+#include <string>
 
 namespace ezsrv::net {
     struct request_message {
         request_message_header header;
         std::string_view       body;
+    };
+
+    struct response_message {
+        response_message_header header;
+        std::string body;
     };
 } // namespace ezsrv::net
 
