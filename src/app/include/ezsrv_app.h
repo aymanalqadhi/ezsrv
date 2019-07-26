d1ae043b30783b8ce0bd3adf48810f6d9ca2cfa1
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-26 22:07:04 +0300

    Accept a system_commands_container via constructor, and pass it to the created server instance

--- src/app/include/ezsrv_app.h
+++ src/app/include/ezsrv_app.h
@@ -3,22 +3,26 @@
 
 #include "iapplication.h"
 
+#include "commands/system_commands.h"
 #include "config/app_config.h"
 #include "log/logger.h"
 #include "net/server.h"
 
 namespace ezsrv::app {
     namespace details {
+        using ezsrv::commands::system_commands_container;
         using ezsrv::config::app_config;
         using ezsrv::log::logger;
         using ezsrv::net::server;
 
         class ezsrv_app final : public iapplication {
           public:
-            explicit ezsrv_app(app_config &config, logger &logger)
+            ezsrv_app(app_config &               config,
+                      logger &                   logger,
+                      system_commands_container &system_commands)
                 : config_ {config},
                   logger_ {logger},
-                  server_ {config, logger} {}
+                  server_ {config, logger, system_commands} {}
 
             void configure() override final;
             void run() override final;
