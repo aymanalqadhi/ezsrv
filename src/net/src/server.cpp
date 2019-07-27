37642726ea29f848d1f7a13da947e4729f1f8e2b
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-28 00:25:33 +0300

    Use the new system_command_error and system_command_result names, and handle invalid system command requests

--- src/net/src/server.cpp
+++ src/net/src/server.cpp
@@ -9,7 +9,7 @@
 
 #include <type_traits>
 
-using ezsrv::commands::result_code;
+using ezsrv::commands::system_command_error;
 using ezsrv::commands::system_commands;
 
 using ezsrv::net::response_message;
@@ -40,7 +40,7 @@ void server::on_client_accepted(tcp_client_ptr client) {
 
 void server::on_request(const tcp_client_ptr &client, request_message req) {
     // TODO:
-    // 1. Validate the command existance
+    // 1. Validate the command existance - Done
     // 2. Send real flags
     // 2. Handle the rest of request types
 
@@ -48,17 +48,31 @@ void server::on_request(const tcp_client_ptr &client, request_message req) {
     case message_type::system_command: {
         auto cmd {system_commands_.get_command(
             static_cast<system_commands>(req.header.extra))};
-        auto cmd_ret {std::invoke(*cmd, req.body)};
 
-        response_message_header header {
-            req.header.type,
-            static_cast<std::underlying_type_t<result_code>>(cmd_ret.code),
-            req.header.seq_no, 0,
-            static_cast<std::uint32_t>(cmd_ret.message.size())};
+        if (cmd == nullptr) {
+            response_message_header header {
+                req.header.type,
+                static_cast<std::underlying_type_t<system_command_error>>(
+                    system_command_error::unknown_command),
+                req.header.seq_no, 0, 0};
 
-        client->enqueue_send(
-            std::shared_ptr<response_message>(new response_message {
-                std::move(header), std::move(cmd_ret.message)}));
+            client->enqueue_send(std::shared_ptr<response_message>(
+                new response_message {std::move(header)}));
+        } else {
+            auto cmd_ret {std::invoke(*cmd, req.body)};
+
+            response_message_header header {
+                req.header.type,
+                static_cast<std::underlying_type_t<system_command_error>>(
+                    cmd_ret.code),
+                req.header.seq_no, 0,
+                static_cast<std::uint32_t>(cmd_ret.message.size())};
+
+            client->enqueue_send(
+                std::shared_ptr<response_message>(new response_message {
+                    std::move(header), std::move(cmd_ret.message)}));
+        }
+
         client->send_enqueued();
 
         break;
