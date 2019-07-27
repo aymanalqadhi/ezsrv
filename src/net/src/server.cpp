2695c7b2f9ff55066ab466677a7aac735f656811
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-28 00:50:15 +0300

    Use the new system commands handlers instead of the current handling code

--- src/net/src/server.cpp
+++ src/net/src/server.cpp
@@ -40,43 +40,11 @@ void server::on_client_accepted(tcp_client_ptr client) {
 
 void server::on_request(const tcp_client_ptr &client, request_message req) {
     // TODO:
-    // 1. Validate the command existance - Done
-    // 2. Send real flags
-    // 2. Handle the rest of request types
-
+    // - Handle the rest of request types
     switch (req.header.type) {
-    case message_type::system_command: {
-        auto cmd {system_commands_.get_command(
-            static_cast<system_commands>(req.header.extra))};
-
-        if (cmd == nullptr) {
-            response_message_header header {
-                req.header.type,
-                static_cast<std::underlying_type_t<system_command_error>>(
-                    system_command_error::unknown_command),
-                req.header.seq_no, 0, 0};
-
-            client->enqueue_send(std::shared_ptr<response_message>(
-                new response_message {std::move(header)}));
-        } else {
-            auto cmd_ret {std::invoke(*cmd, req.body)};
-
-            response_message_header header {
-                req.header.type,
-                static_cast<std::underlying_type_t<system_command_error>>(
-                    cmd_ret.code),
-                req.header.seq_no, 0,
-                static_cast<std::uint32_t>(cmd_ret.message.size())};
-
-            client->enqueue_send(
-                std::shared_ptr<response_message>(new response_message {
-                    std::move(header), std::move(cmd_ret.message)}));
-        }
-
-        client->send_enqueued();
-
+    case message_type::system_command:
+        handle_system_command(client, req);
         break;
-    }
     default:
         logger_.info("Got message from client #{}: {}", client->id(), req.body);
     }
