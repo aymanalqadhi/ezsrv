5b0f80330757ca2bd1f6117d0386bd66c45b3e85
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-28 22:41:57 +0300

    Add base service abstract class

---
+++ src/services/include/services/service.h
@@ -0,0 +1,16 @@
+#ifndef EZSRV_SERVICES_SERVICE_H
+#define EZSRV_SERVICES_SERVICE_H
+
+#include <string_view>
+
+namespace ezsrv::services {
+    class service_base {
+      public:
+        virtual const std::string_view &display_name() const noexcept = 0;
+        virtual bool                    is_required() const noexcept  = 0;
+
+        virtual void initialize() = 0;
+    };
+} // namespace ezsrv::services
+
+#endif /* EZSRV_SERVICES_SERVICE_H */
