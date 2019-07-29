6d321784d6467ca9f243d4f637e51206532c7699
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-29 21:18:56 +0300

    Accept an echo_command services reference via the constructor

--- src/commands/include/commands/echo_command.h
+++ src/commands/include/commands/echo_command.h
@@ -3,11 +3,20 @@
 
 #include "commands/system_commands.h"
 
+#include "services/echo_service.h"
+
 namespace ezsrv::commands {
     class echo_command final : public ezsrv::commands::system_command {
+      public:
+        echo_command(ezsrv::services::echo_service &echo_service)
+            : echo_service_ {echo_service} {}
+
         ezsrv::commands::system_command_result
         operator()(std::string_view msg) override final;
-    };
-} // namespace ezsrv::commands::detail
 
-#endif /* end of include guard: EZSRV_COMMANDS_DETAIL_ECHO_COMMAND_H */
+      private:
+        ezsrv::services::echo_service echo_service_;
+    };
+} // namespace ezsrv::commands
+
+#endif /* EZSRV_COMMANDS_DETAIL_ECHO_COMMAND_H */
