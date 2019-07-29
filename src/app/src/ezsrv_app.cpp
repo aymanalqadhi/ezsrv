c9e5dc923abcbfe474e6bf8a0f7c3ff9374e6e73
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2019-07-29 20:03:35 +0300

    Initialize services using the helper class system_services_container

--- src/app/src/ezsrv_app.cpp
+++ src/app/src/ezsrv_app.cpp
@@ -7,6 +7,11 @@ using ezsrv::app::ezsrv_app;
 
 void ezsrv_app::configure() {
     logger_.info("Configuring application {}", config_.executable);
+
+    logger_.info("Initializng system services");
+    if (!system_services_.initialize()) {
+        throw std::runtime_error {"Could not initialize system services"};
+    }
 }
 
 void ezsrv_app::run() {
